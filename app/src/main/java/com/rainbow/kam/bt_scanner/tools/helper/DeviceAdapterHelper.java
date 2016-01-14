5f42f6941c188bb3f2298a7dd0c54a77b975bd3e
kam6512_rainbow <kam6512@gmail.com>
2016-01-14 11:44:38 +0900

    Test DeviceAdapterHelper entrySet

--- app/src/main/java/com/rainbow/kam/bt_scanner/tools/helper/DeviceAdapterHelper.java
+++ app/src/main/java/com/rainbow/kam/bt_scanner/tools/helper/DeviceAdapterHelper.java
@@ -1,25 +1,33 @@
 package com.rainbow.kam.bt_scanner.tools.helper;
 
+import android.util.Log;
+
 import com.rainbow.kam.bt_scanner.adapter.DeviceItem;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
+import hugo.weaving.DebugLog;
+
 /**
  * Created by kam6512 on 2016-01-08.
  */
 public class DeviceAdapterHelper {
 
-
-    public static  DeviceItem getValue(LinkedHashMap<String, DeviceItem> hashMap, int position) {
-        Map.Entry<String, DeviceItem> deviceItemEntry=null;
+    public static DeviceItem getValue(LinkedHashMap<String, DeviceItem> hashMap, int position) {
+        Map.Entry<String, DeviceItem> deviceItemEntry = null;
         Set<Map.Entry<String, DeviceItem>> entries = hashMap.entrySet();
         int index = 0;
         for (Map.Entry<String, DeviceItem> entry : entries) {
             if (index++ == position) {
                 deviceItemEntry = entry;
             }
+            Log.i("getValue Entry", entry.getValue().getExtraAddress());
+        }
+
+        for (DeviceItem deviceItem : hashMap.values()) {
+            Log.i("getValue DeviceItem", deviceItem.getExtraAddress());
         }
         return deviceItemEntry.getValue();
     }
